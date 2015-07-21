285227fa003569c5aa41b0b902202258d6b9fdae
Austin Borger <aaborger@gmail.com>
2015-07-20 22:32:34 -0400

    Updated project files

---
+++ StereoFFT_Test/Main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#include "App.h"
+
+#pragma comment(lib, "DXAudio.lib")
+#pragma comment(lib, "DXWindow.lib")
+#pragma comment(lib, "StereoFFT.lib")
+#pragma comment(lib, "d3d11.lib")
+
+int APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) {
+	App app;
+
+	return app.Run();
+}
